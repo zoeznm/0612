4d6f5c907437722e213718df158af61fe169ad3b
zoeznm <bomikim046@gmail.com>
2024-06-12 10:31:05 +0900

    file

---
+++ app.js
@@ -0,0 +1,13 @@
+const basicData = {
+  name : "",
+  age: 0,
+  job : ""
+}
+
+class BasicData {
+  constructor(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+  }
+}
---
+++ work.js
@@ -0,0 +1,9 @@
+const basicData = {
+  name : '',
+  age : 0,
+  job : ''
+}
+
+const component = () => {
+  
+}
